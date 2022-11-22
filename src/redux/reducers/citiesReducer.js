04169e7a54ecbb446b3db74506e7cabf2040688c
Your Name01 <rocioduarte197@gmail.com>
2022-11-22 04:56:51 -0300

    mt-01-41

--- src/redux/reducers/citiesReducer.js
+++ src/redux/reducers/citiesReducer.js
@@ -1,10 +1,11 @@
 import { createReducer } from "@reduxjs/toolkit";
 import citiesAction from "../actions/citiesAction"
 
-const {getCities,filterCheckCities,createNewCity} = citiesAction
+const {getCities,filterCheckCities,createNewCity, getCitiesAdmi, deleteCitiesAdmi,updateCitiesAdmi} = citiesAction
 
 const initialState = {
     listCities:[],
+    admiCities: [],
     continent:[],
     search: '',
     checkBox: '',
@@ -25,6 +26,16 @@ const citiesReducer = createReducer(initialState, (builder) => {
             if(action.payload.success){
                 state.listCities.push(action.payload.response)
             }
+        })
+        .addCase(getCitiesAdmi.fulfilled, (state, action) => {
+            return{...state, admiCities: action.payload}
+        })
+        .addCase(deleteCitiesAdmi.fulfilled, (state, action) => {
+            let cities = state.admiCities.filter(cities => cities.id !== action.data._id)
+            return{...state, admiCities: cities}
+        })
+        .addCase(updateCitiesAdmi.fulfilled, (state, action) => {
+            return{...state}
         })
 });
 
